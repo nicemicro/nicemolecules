a0405039481f5d4273c04e9e60fe446be2ec0018
nicemicro <nicemicro@protonmail.com>
2022-02-24 18:28:36 +0900

    list all atoms in a molecule

--- mol_eng.py
+++ mol_eng.py
@@ -5,7 +5,7 @@ Created on Mon Feb 21 15:52:19 2022
 @author: nicemicro
 """
 
-class cov_bond:
+class Cov_bond:
     def __init__(self, atoms, electrons):
         self.atoms = atoms
         self.electrons = electrons
@@ -36,7 +36,7 @@ class cov_bond:
             return False
         return list(atom for atom in self.atoms if atom != one_atom)
 
-class atom:
+class Atom:
     def __init__(self, letter, valence_el, coords, charge=0, fullshell=8,
                  hypervalent=False):
         self.letter = letter
@@ -60,7 +60,7 @@ class atom:
         for bond in self.bonds:
             desc += f"    {bond.order()} order bond with "
             for atom in bond.other_atoms(self):
-                desc += f"{atom.letter} "
+                desc += f"{atom.letter} at ({atom.coord_x}, {atom.coord_y})"
             desc += "\n"
         return desc
     
@@ -76,7 +76,7 @@ class atom:
         if other_atom.is_bonded(self):
             self.bonds.append(other_atom.bond_instance(self))
         else:
-            new_bond = cov_bond([self, other_atom], [order-dative, order+dative])
+            new_bond = Cov_bond([self, other_atom], [order-dative, order+dative])
             self.bonds.append(new_bond)
             other_atom.bond(self, order, -dative)
         return True
@@ -84,6 +84,15 @@ class atom:
     def is_bonded(self, other_atom):
         bond_list_lists = list(bond.other_atoms(self) for bond in self.bonds)
         return other_atom in [item for sublist in bond_list_lists for item in sublist]
+    
+    def bonded_atoms(self):
+        bond_list_lists = list(bond.other_atoms(self) for bond in self.bonds)
+        bond_list = [item for sublist in bond_list_lists for item in sublist]
+        atomlist = []
+        for atom in bond_list:
+            if not atom in atomlist:
+                atomlist.append(atom)
+        return atomlist
     
     def bond_instance(self, other_atom):
         for bond in self.bonds:
@@ -99,9 +108,20 @@ class atom:
             from_bonds -= bond.atom_electrons(self)
         return from_bonds + self.valence
 
+def find_molecule(one_atom):
+    atomlist = [one_atom]
+    current_num = 0
+    while current_num < len(atomlist):
+        current_atom = atomlist[current_num]
+        for bonded_atom in current_atom.bonded_atoms():
+            if not bonded_atom in atomlist:
+                atomlist.append(bonded_atom )
+        current_num += 1
+    return atomlist
+
 def test1():
-    a = atom("C", 4, [0, 0], 0)
-    b = atom("O", 6, [1, 0], 0)
+    a = Atom("C", 4, [0, 0], 0)
+    b = Atom("O", 6, [1, 0], 0)
     print(a)
     print(b)
     print("bond a to b: ", a.bond(b, 3, 1))
@@ -111,11 +131,11 @@ def test1():
     print(a.bonds)
 
 def test2():
-    a = atom("C", 4, [1, 1])
-    b = atom("H", 1, [0, 1], fullshell=2)
-    c = atom("H", 1, [1, 0], fullshell=2)
-    d = atom("H", 1, [1, 2], fullshell=2)
-    e = atom("H", 1, [2, 1], fullshell=2)
+    a = Atom("C", 4, [1, 1])
+    b = Atom("H", 1, [0, 1], fullshell=2)
+    c = Atom("H", 1, [1, 0], fullshell=2)
+    d = Atom("H", 1, [1, 2], fullshell=2)
+    e = Atom("H", 1, [2, 1], fullshell=2)
     a.bond(b)
     a.bond(c)
     a.bond(d)
@@ -123,5 +143,32 @@ def test2():
     print(a)
     print(a.bonds)
 
-test2()
-    
+def test3():
+    molecules = []
+    molecules.append(Atom("C", 4, [1, 1]))
+    for index in range(4):
+        molecules.append(Atom("H", 1, [index % 2, index // 2], fullshell=2))
+        molecules[-1].bond(molecules[0])
+    carbons = []
+    hydrogens = []
+    for index in range(3):
+        carbons.append(Atom("C", 4, [6 + index*2, 2]))
+        if len(carbons) > 1:
+            carbons[-1].bond(carbons[-2])
+        for index2 in range(2):
+            hydrogens.append(Atom("H", 1, [6 + index*2, index2 * 4], fullshell=2))
+            hydrogens[-1].bond(carbons[-1])
+    hydrogens.append(Atom("H", 1, [12, 2], fullshell=2))
+    hydrogens[-1].bond(carbons[-1])
+    hydrogens.append(Atom("H", 1, [4, 2], fullshell=2))
+    hydrogens[-1].bond(carbons[0])
+    for atom_member in find_molecule(hydrogens[3]):
+        print(f"{atom_member.letter} ({atom_member.coord_x}, {atom_member.coord_y})")
+    print()
+    for atom_member in find_molecule(molecules[1]):
+        print(f"{atom_member.letter} ({atom_member.coord_x}, {atom_member.coord_y})")
+    print()
+    for atom_member in find_molecule(carbons[0]):
+        print(f"{atom_member.letter} ({atom_member.coord_x}, {atom_member.coord_y})")
+
+test3()
