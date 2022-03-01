8c60aa474636c748b792ded57c8f2549c15f5e37
nicemicro <63175285+nicemicro@users.noreply.github.com>
2022-03-02 08:50:40 +0900

    show possible new linked atoms if clicked on atom

--- mol_gui.py
+++ mol_gui.py
@@ -10,6 +10,16 @@ import tkinter as tk
 from tkinter import ttk
 from enum import IntEnum, auto
 
+MINDIST = 20
+DEFLEN = 30
+cos30 = DEFLEN * 3 ** (1/2) / 2
+XSHIFT = [0, 0, DEFLEN, -DEFLEN,
+          DEFLEN / 2, DEFLEN / 2, -DEFLEN / 2, -DEFLEN / 2,
+          cos30, -cos30, cos30, -cos30]
+YSHIFT = [DEFLEN, -DEFLEN, 0, 0,
+          cos30, -cos30, cos30, -cos30,
+          DEFLEN / 2, DEFLEN / 2, -DEFLEN / 2, -DEFLEN / 2]
+
 class TopToolbar(ttk.Frame):
     class Modes(IntEnum):
         SELECT = auto()
@@ -44,6 +54,10 @@ class TopToolbar(ttk.Frame):
         self.atom = atom
 
 class AppContainer(tk.Tk):
+    class Modes(IntEnum):
+        NORMAL = auto()
+        ADD_LINKED_ATOM = auto()
+    
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.atoms = []
@@ -51,6 +65,7 @@ class AppContainer(tk.Tk):
         self.graphics = {}
         self.selected = None
         self.event_listened = False
+        self.mode = self.Modes.NORMAL
         self.title("Nice Molecules")
         
         container = ttk.Frame(self)
@@ -64,6 +79,9 @@ class AppContainer(tk.Tk):
         self.mol_canvas = tk.Canvas(container, width=700, height=600)
         self.mol_canvas.grid(row=1, column=0, sticky="nsew")
         self.mol_canvas.bind("<Button-1>", self.leftclick_canvas)
+        
+        #self.atoms, self.bonds = eng.test3()
+        #self.redraw_all_molecules(self.atoms, self.bonds)
     
     def leftclick_canvas(self, event):
         if self.event_listened:
@@ -80,11 +98,10 @@ class AppContainer(tk.Tk):
                 item = None
             else:
                 item = closestitems[0]
-            print(item)
             close = item in self.graphics and \
                 isinstance(self.graphics[item], eng.Atom) and \
-                -20 < self.graphics[item].coord_x - event.x < 20 and \
-                -20 < self.graphics[item].coord_y - event.y < 20
+                -MINDIST < self.graphics[item].coord_x - event.x < MINDIST and \
+                -MINDIST < self.graphics[item].coord_y - event.y < MINDIST
             if close:
                 return
             coords = [event.x, event.y]
@@ -100,6 +117,36 @@ class AppContainer(tk.Tk):
             return
         if self.toolbar.what_add():
             self.event_listened = True
+            self.possible_atoms(self.graphics[atom_s])
+            self.mode =self.Modes.ADD_LINKED_ATOM
+    
+    def mouseup_atom(self, atom_s, event):
+        self.mol_canvas.delete("ui_help")
+        self.mode =self.Modes.NORMAL
+    
+    def drag_atom(self, atom_s, event):
+        if self.mode == self.Modes.ADD_LINKED_ATOM:
+            closestitems = self.mol_canvas.find_closest(event.x, event.y)
+            self.mol_canvas.itemconfigure("ui_help", fill="grey")
+            if len(closestitems) == 0:
+                return
+            item = closestitems[0]
+            tags = self.mol_canvas.gettags(item)
+            if len(tags) == 0 or tags[0] != "ui_help":
+                return
+            self.mol_canvas.itemconfigure(tags[1], fill="blue")
+    
+    def possible_atoms(self, atom):
+        num = 1
+        for (deltax, deltay) in zip(XSHIFT, YSHIFT):
+            self.draw_bond(atom.coord_x, atom.coord_y, atom.coord_x+deltax,
+                           atom.coord_y+deltay, 30, color="grey",
+                           tags=("ui_help", f"ui_help_{num}"))
+            self.mol_canvas.create_text(atom.coord_x+deltax, atom.coord_y+deltay,
+                                        text=self.toolbar.what_add(),
+                                        justify="center", fill="grey",
+                                        tags=("ui_help", f"ui_help_{num}"))
+            num += 1
     
     def add_atom(self, atom_symbol, coords):
         new_atom = eng.add_atom_by_symbol(atom_symbol, coords)
@@ -107,11 +154,38 @@ class AppContainer(tk.Tk):
         self.atoms.append(new_atom)
         self.redraw_all_molecules(self.atoms, self.bonds)
     
+    def draw_bond(self, x1, y1, x2, y2, bondlen, order=1, dativity=0,
+                  color="black", tags=None):
+        if tags is None:
+            tags = ()
+        obscure = 7
+        shift = 2
+        bondshifts = list(range(1-order, order+1, 2))
+        bond_objects = []
+        for bondpart in bondshifts:
+            xd1 = x1 + (obscure / bondlen) * (x2-x1)
+            xd2 = x2 - (obscure / bondlen) * (x2-x1)
+            yd1 = y1 + (obscure / bondlen) * (y2-y1)
+            yd2 = y2 - (obscure / bondlen) * (y2-y1)
+            xs = (y1 - y2) / bondlen * shift * bondpart
+            ys = (x2 - x1) / bondlen * shift * bondpart
+            if dativity == 0:
+                arrowtype = None
+            elif dativity < 0:
+                arrowtype = "first"
+                dativity += 1
+            elif dativity > 0:
+                arrowtype = "last"
+                dativity -= 1
+            bondid = self.mol_canvas.create_line(xd1+xs, yd1+ys, xd2+xs, yd2+ys,
+                                                 arrow=arrowtype, fill=color,
+                                                 tags=tags)
+            bond_objects.append(bondid)
+        return bond_objects
+    
     def redraw_all_molecules(self, atomlist, bondlist):
         self.mol_canvas.delete("all")
         self.graphics = {}
-        obscure = 7
-        shift = 2
         for bond in bondlist:
             x1, y1, x2, y2 = bond.coords()
             bondlen = bond.length()
@@ -119,25 +193,9 @@ class AppContainer(tk.Tk):
                 continue
             bond_order = int(bond.order())
             dativity = bond.dativity()
-            bondshifts = list(range(1-bond_order, bond_order+1, 2))
-            for bondpart in bondshifts:
-                xd1 = x1 + (obscure / bondlen) * (x2-x1)
-                xd2 = x2 - (obscure / bondlen) * (x2-x1)
-                yd1 = y1 + (obscure / bondlen) * (y2-y1)
-                yd2 = y2 - (obscure / bondlen) * (y2-y1)
-                xs = (y1 - y2) / bondlen * shift * bondpart
-                ys = (x2 - x1) / bondlen * shift * bondpart
-                if dativity == 0:
-                    arrowtype = None
-                elif dativity < 0:
-                    arrowtype = "first"
-                    dativity += 1
-                elif dativity > 0:
-                    arrowtype = "last"
-                    dativity -= 1
-                bondid = self.mol_canvas.create_line(xd1+xs, yd1+ys,
-                                                     xd2+xs, yd2+ys,
-                                                     arrow=arrowtype)
+            bond_drawings = self.draw_bond(x1, y1, x2, y2, bondlen, 
+                                           bond_order, dativity)
+            for bondid in bond_drawings:
                 self.graphics[bondid] = bond
         for atom in atomlist:
             atom_s = self.mol_canvas.create_text(atom.coord_x, atom.coord_y,
@@ -147,6 +205,12 @@ class AppContainer(tk.Tk):
             self.mol_canvas.tag_bind(atom_s, "<Button-1>",
                                      lambda event, atom_s=atom_s:
                                          self.leftclick_atom(atom_s, event))
+            self.mol_canvas.tag_bind(atom_s, "<ButtonRelease-1>",
+                                     lambda event, atom_s=atom_s:
+                                         self.mouseup_atom(atom_s, event))
+            self.mol_canvas.tag_bind(atom_s, "<B1-Motion>",
+                                     lambda event, atom_s=atom_s:
+                                         self.drag_atom(atom_s, event))
         
 def main():
     app = AppContainer()
