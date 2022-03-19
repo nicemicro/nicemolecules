9c273e8c0634f39e2c85d47f2e78b10ea12c02fd
nicemicro <63175285+nicemicro@users.noreply.github.com>
2022-03-19 23:02:49 +0900

    Elements have immutable attributes

--- elements.py
+++ elements.py
@@ -11,65 +11,87 @@ class Element:
     """
     Abstract class that stores information about an element.
     Members:
-        symbol: str, the chemical symbol of an atom
-        valence_el: int, number of valence electrons
-        fullshell: int, the number of electrons in a filled valence shell
-        hypervalent: bool, whether the atom can accomodate more electrons
+        _symbol: str, the chemical symbol of an atom
+        _valence_el: int, number of valence electrons
+        _fullshell: int, the number of electrons in a filled valence shell
+        _hypervalent: bool, whether the atom can accomodate more electrons
             then the full valence shell
     """
-    symbol: str = ""
-    valence_el: int = 0
-    fullshell: int = 8
-    hypervalent: bool = False
+    _symbol: str = ""
+    _valence_el: int = 0
+    _fullshell: int = 8
+    _hypervalent: bool = False
 
+    def get_symbol(self):
+        return self._symbol
+
+    def get_valence_el(self):
+        return self._valence_el
+
+    def get_fullshell(self):
+        return self._fullshell
+
+    def get_hypervalent(self):
+        return self._hypervalent
+
+    def set_attr (self, new):
+        raise AttributeError("Can't modify element properties")
+
+    def del_attr(self):
+        raise AttributeError("Can't delete element properties")
+
+    symbol = property(get_symbol, set_attr, del_attr)
+    valence_el = property(get_valence_el, set_attr, del_attr)
+    fullshell = property(get_fullshell, set_attr, del_attr)
+    hypervalent = property(get_hypervalent, set_attr, del_attr)
 
 class Hydrogen(Element):
-    symbol = "H"
-    valence_el = 1
-    fullshell = 2
+    _symbol = "H"
+    _valence_el = 1
+    _fullshell = 2
 
 
 class Boron(Element):
-    symbol = "B"
-    valence_el = 3
+    _symbol = "B"
+    _valence_el = 3
 
 
 class Carbon(Element):
-    symbol = "C"
-    valence_el = 4
+    _symbol = "C"
+    _valence_el = 4
 
 
 class Nitrogen(Element):
-    symbol = "N"
-    valence_el = 5
+    _symbol = "N"
+    _valence_el = 5
 
 
 class Oxygen(Element):
-    symbol = "O"
-    valence_el = 6
+    _symbol = "O"
+    _valence_el = 6
 
 
 class Fluorine(Element):
-    symbol = "F"
-    valence_el = 7
+    _symbol = "F"
+    _valence_el = 7
 
 
 class Silicon(Element):
-    symbol = "Si"
-    valence_el = 4
-    hypervalent = True
+    _symbol = "Si"
+    _valence_el = 4
+    _hypervalent = True
 
 
 class Phosphorus(Element):
-    symbol = "P"
-    valence_el = 5
-    hypervalent = True
+    _symbol = "P"
+    _valence_el = 5
+    _hypervalent = True
 
 
 class Sulphur(Element):
-    symbol = "S"
-    valence_el = 6
-    hypervalent = True
+    _symbol = "S"
+    _valence_el = 6
+    _hypervalent = True
 
 
 element_table: list[Element] = []
